bfeb717248e5451d671238c4d2ade1750699909e
iftikarjahan <iftikarjahan22@gmail.com>
2024-12-04 21:34:36 +0530

    add the create user functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
-const registerController=(req,res,next)=>{
-    res.send("register user")
+const {StatusCodes}=require("http-status-codes");
+const User=require("../models/User");
+
+const registerController=async(req,res,next)=>{
+    
+    const createdUser=await User.create({...req.body});
+    res.status(StatusCodes.CREATED).json(createdUser);
 }
 
 const loginController=(req,res,next)=>{
