3459a19b92ec5821e0fb9b436c6df04ecad6c8ae
iftikarjahan <iftikarjahan22@gmail.com>
2024-12-08 22:02:49 +0530

    added the job schema

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const { ref } = require("joi");
+const mongoose=require("mongoose");
+
+const jobSchema=mongoose.Schema({
+    company:{
+        type:String,
+        required:[true,"Please provide the company name"],
+        maxlength:50
+    },
+    position:{
+        type:String,
+        required:[true,"Please provide the position"],
+        maxlength:50
+    },
+    status:{
+        type:String,
+        enum:["interview","pending","declined"],
+        default:"pending"
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+        required:[true,"Please provide the user"]
+    },
+},{timestamps:true})
+
+module.exports=mongoose.Model("Job",jobSchema);
