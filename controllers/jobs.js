e25b76a6a23f18cb308511ee2a25f41b0ed9dd31
iftikarjahan <iftikarjahan22@gmail.com>
2024-12-09 20:33:09 +0530

    added the getSingleJob controller/functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const Job=require("../models/Job");
 const {StatusCodes}=require("http-status-codes");
+const {NotFoundError}=require("../errors/not-found");
 
 
 
@@ -20,8 +21,29 @@ const getAllJobsController=async (req,res,next)=>{
     res.status(StatusCodes.OK).json({jobs,noOfJobs:jobs.length});
 }
 
-const getSingleJobController=(req,res,next)=>{
-    res.send("get single job");
+const getSingleJobController=async (req,res,next)=>{
+    
+    // used js destructuring syntax
+    const{user:{userId,userName},params:{id:jobId}}=req;
+    // based on the jobId and userId, you need to find the corresponding job
+    /*
+    ->Note an important thing that ids in mongodb are unique. So even if we use only one 
+    filter like jobId as the filter, then also we would get the job.
+    ->But still we are using the createdBy as an extra filter. This is because of security 
+    reasons
+    ->If any other user gets a jobId created by some other user, then he can easily
+    access that job even though he has not created the job.
+    ->So by adding the extra createdBy filter, we ensure that the logged in user is 
+    only allowed to access that job
+    */ 
+    const job=await Job.findById({_id:jobId,createdBy:userId});
+    // console.log(job);
+    if(!job){
+        // console.log("lllllllll");
+        throw new NotFoundError("Job with this id not found🥶");
+    }
+    res.status(StatusCodes.OK).json({job});
+    
 }
 
 // update
